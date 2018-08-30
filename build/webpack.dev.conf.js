7be0f2982ad0e10c92f51dd7ce30de46c97a656f
candy <1964646121@qq.com>
2018-08-30 16:12:13 +0800

    mock数据

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,8 +10,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
+//mock数据
+const express = require('express')
+const app = express()
+const apiRouters = express.Router()
+const appData = require('../data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+app.use('/api',apiRouters);
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
+
+
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -42,6 +54,26 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    before(app) {
+      app.get('/api/seller', (req, res) => {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      }),
+        app.get('/api/goods', (req, res) => {
+          res.json({
+            errno: 0,
+            data: goods
+          })
+        }),
+        app.get('/api/ratings', (req, res) => {
+          res.json({
+            errno: 0,
+            data: ratings
+          })
+        })
     }
   },
   plugins: [
