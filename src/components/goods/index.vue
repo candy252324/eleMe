3cee487ecb3d71b488c52886148aaeeac4886c6b
candy <1964646121@qq.com>
2018-08-31 19:50:44 +0800

    使用betterSroll实现滚动及菜单联动

--- src/components/goods/index.vue
+++ src/components/goods/index.vue
@@ -10,7 +10,7 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper" ref="foodWrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
         <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
@@ -43,6 +43,7 @@
 </template>
 
 <script>
+  import BScroll from 'better-scroll'
   const ERR_OK=0;
 
   export default {
@@ -50,7 +51,31 @@
     data() {
       return {
         goods:[],
-        currentIndex:undefined,
+        listHeight:[],  // 每个区间的高度
+        scrollY: 0,  // 右侧区域滑动的高度
+      }
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      },
+      selectFoods() {
+        let foods = [];
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if (food.count) {
+              foods.push(food);
+            }
+          });
+        });
+        return foods;
       }
     },
     created(){
@@ -58,12 +83,47 @@
         let d=res.data
         if(d.errno===ERR_OK){
           this.goods=d.data;
+          this.$nextTick(()=>{
+            this._initScroll();
+            this._calculateHeight();
+          })
         }
       })
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     },
     methods:{
-      selectMenu(){},
+      _initScroll(){
+        this.menuScroll=new BScroll(this.$refs.menuWrapper,{
+          click: true,   // click:true,向外派发点击事件，但是会导致pc端响应两次点击事件
+        })
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          probeType: 3,
+          click: true
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight(){
+        let foodsList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodsList.length; i++) {
+          let item = foodsList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+      selectMenu(index,event){
+        // betterScroll派发的click事件,event有_constructed属性，而浏览器原生的click事件没有该属性
+        // 从而规避了pc端响应两次点击事件的问题(实际上似乎并不存在这个问题？？？)
+        if (!event._constructed) {
+          return;
+        }
+        let foodsList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let el = foodsList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      },
       selectFood(){},
     }
   }
