7178bc51deab5577fd537a15c57f852fadc9a5ab
candy <1964646121@qq.com>
2018-08-31 10:42:02 +0800

    公告栏完成

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -22,47 +22,48 @@
         <i class="icon icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <!--<div class="bulletin-wrapper" @click="showDetail">-->
-      <!--<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>-->
-      <!--<i class="icon iconfont icon-zuoyoujiantou"></i>-->
-    <!--</div>-->
-    <!--<div class="background">-->
-      <!--<img :src="seller.avatar" alt="" class="" width="100%" height="100%">-->
-    <!--</div>-->
-    <!--<transition name="fade">-->
-      <!--<div v-show="detailShow" class="detail" @click="hideDetail" transition="fade">-->
-        <!--<div class="detail-wrapper clearFix">-->
-          <!--<div class="detail-main">-->
-            <!--<h1 class="name">{{seller.name}}</h1>-->
-            <!--<div class="star-wrapper">-->
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" alt="" class="" width="100%" height="100%">
+    </div>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail" @click="hideDetail" transition="fade">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              123
               <!--<star :size="48" :score="seller.score"></star>-->
-            <!--</div>-->
-            <!--<div class="title">-->
-              <!--<div class="line"></div>-->
-              <!--<div class="text">优惠信息</div>-->
-              <!--<div class="line"></div>-->
-            <!--</div>-->
-            <!--<ul v-if="seller.supports" class="supports">-->
-              <!--<li class="support-item" v-for="(item, index) in seller.supports">-->
-                <!--<span class="icon" :class="classMap[seller.supports[index].type]"></span>-->
-                <!--<span class="text">{{seller.supports[index].description}}</span>-->
-              <!--</li>-->
-            <!--</ul>-->
-            <!--<div class="title">-->
-              <!--<div class="line"></div>-->
-              <!--<div class="text">商家公告</div>-->
-              <!--<div class="line"></div>-->
-            <!--</div>-->
-            <!--<div class="bulletin">-->
-              <!--<p class="content">{{seller.bulletin}}</p>-->
-            <!--</div>-->
-          <!--</div>-->
-        <!--</div>-->
-        <!--<div class="detail-close" @click="hideDetail">-->
-          <!--<i class="iconfont icon-cha"></i>-->
-        <!--</div>-->
-      <!--</div>-->
-    <!--</transition>-->
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item, index) in seller.supports">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon icon-close"></i>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -74,13 +75,20 @@
       }
     },
     data() {
-      return {}
+      return {
+        detailShow:false,
+      }
     },
     created(){
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     },
     methods:{
-      showDetail(){}
+      showDetail(){
+        this.detailShow=true;
+      },
+      hideDetail(){
+        this.detailShow=false;
+      },
     }
   }
 </script>
@@ -154,12 +162,156 @@
         line-height 24px
         border-radius 12px
         background rgba(0, 0, 0, 0.2)
-        text-align content
+        text-align center
         .count
           font-size 10px
           vertical-align top
-        .icon.icon-keyboard_arrow_right
+        .icon
           margin-left 2px
           line-height 24px
           font-size 10px
+    .bulletin-wrapper
+      position relative
+      background-color rgba(7, 17, 27, 0.2)
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 8px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top
+        margin 0 4px
+        font-size 10px
+      .icon
+        position absolute
+        font-size 20px
+        right 12px
+        top: 4px
+    .background
+      position absolute
+      top: 0
+      left 0
+      width 100%
+      z-index -1
+      filter blur(10px)
+    .detail
+      position fixed
+      z-index 100
+      top 0
+      right 0
+      width 100%
+      height 100%
+      overflow auto
+      background rgba(7, 17, 27, 0.8)
+      -webkit-backdrop-filter blur(10px)
+      &.fade-enter-active
+        animation: bounce-in .5s;
+      &.fade-leave-active
+        animation: bounce-out .5s;
+      @keyframes bounce-in {
+        0% {
+          transform: scale(0);
+        }
+        50% {
+          transform: scale(1.5);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+      @keyframes bounce-out {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(1.5);
+        }
+        100% {
+          transform: scale(0);
+        }
+      }
+    .detail-wrapper
+      width 100%
+      min-height 100%
+      .detail-main
+        margin-top 64px
+        padding-bottom 64px
+        .name
+          line-height 16px
+          text-align center
+          font-size 16px
+          font-weight 700px
+        .star-wrapper
+          margin-top 18px
+          padding 2px 0
+          text-align center
+        .title
+          width 80%
+          display flex
+          margin 28px auto 0 auto
+          .line
+            flex: 1
+            position relative
+            top -6px
+            border-bottom 1px solid rgba(255, 255, 255, 0.2)
+          .text
+            padding 0px 12px
+            font-size 16px
+            font-weight 700px
+        .supports
+          width 80%
+          margin 22px auto
+          .support-item
+            padding 0 12px
+            font-size 0px
+            margin-bottom 12px
+            &:last-child
+              margin-bottom 0px
+            .icon
+              display inline-block
+              width 16px
+              height 16px
+              vertical-align top
+              margin-right 6px
+              background-size 16px 16px
+              background-repeat no-repeat
+              &.decrease
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              line-height 12px
+              font-size 12px
+              color #ffffff
+        .bulletin
+          width 80%
+          margin 22px auto
+          .content
+            padding 0 12px
+            line-height 24px
+            font-size 12px
+    .detail-close
+      position relative
+      width 32px
+      height 32px
+      margin -64px auto 0 auto
+      clear both
+      font-size 32px
+
 </style>
